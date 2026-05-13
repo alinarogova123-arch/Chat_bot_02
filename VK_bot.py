d400e4b32d4ac509584c56101b6cf0e9392a14cf
alinarogova123-arch <alinarogova123@gmail.com>
2026-05-13 18:18:34 +0300

    Added answear configuration by flag

--- VK_bot.py
+++ VK_bot.py
@@ -1,9 +1,22 @@
 import random
 
 import vk_api
-from TG_bot import detect_intent_texts
+from google.cloud import dialogflow
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def detect_intent_texts_and_fallback_flag(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    fulfillment_text = response.query_result.fulfillment_text
+    is_fallback = response.query_result.intent.is_fallback
+    return fulfillment_text, is_fallback
 
 
 def echo(message, user_id, vk_api):
@@ -25,8 +38,14 @@ def main():
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            fulfillment_text = detect_intent_texts(project_id, event.user_id, event.text, language_code)
-            echo(fulfillment_text, event.user_id, vk)
+            fulfillment_text, is_fallback = detect_intent_texts_and_fallback_flag(
+                                                project_id,
+                                                event.user_id,
+                                                event.text,
+                                                language_code
+                                            )
+            if not is_fallback:
+                echo(fulfillment_text, event.user_id, vk)
 
 
 if __name__ == "__main__":
