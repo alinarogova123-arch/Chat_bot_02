9bd1ab92de8f6b0031d0d4503be80a6406613104
alinarogova123-arch <alinarogova123@gmail.com>
2026-05-14 19:47:38 +0300

    Update VK_bot.py

--- VK_bot.py
+++ VK_bot.py
@@ -16,15 +16,16 @@ logger = logging.getLogger(__name__)
 class MyLogsHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
-        env = Env()
-        env.read_env()
-        proxy_ip = env.str("PROXY")
-        proxy_url = f'socks5h://{proxy_ip}'
-        apihelper.proxy = {'https': proxy_url}
-        chat_id = env.str("TELEGRAM_CHAT_ID")
-        tg_bot_token = env.str("TELEGRAM_BOT_API_KEY")
-        bot_logger = telebot.TeleBot(tg_bot_token)
-        bot_logger.send_message(chat_id=chat_id, text=log_entry)
+        # env = Env()
+        # env.read_env()
+        # proxy_ip = env.str("PROXY")
+        # proxy_url = f'socks5h://{proxy_ip}'
+        # apihelper.proxy = {'https': proxy_url}
+        # chat_id = env.str("TELEGRAM_CHAT_ID")
+        # tg_bot_token = env.str("TELEGRAM_BOT_API_KEY")
+        # bot_logger = telebot.TeleBot(tg_bot_token)
+        # bot_logger.send_message(chat_id=chat_id, text=log_entry)
+        print(log_entry)
 
 
 def detect_intent_texts_and_fallback_flag(project_id, session_id, text, language_code):
